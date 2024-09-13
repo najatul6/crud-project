15fdd9764f78da960c954bfbe673a5892780a619
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-13 22:44:00 +0600

    update form inputs

--- application/boot/boot/model/person.py
+++ application/boot/boot/model/person.py
@@ -2,7 +2,8 @@ from pweb_orm import PwebModel, pweb_orm
 
 
 class Person(PwebModel):
-    name = pweb_orm.Column("name", pweb_orm.String(150), nullable=False)
+    firstName = pweb_orm.Column("first_name", pweb_orm.String(150), nullable=False)
+    lastName = pweb_orm.Column("last_name", pweb_orm.String(150),)
     email = pweb_orm.Column("email", pweb_orm.String(150), nullable=False)
     password = pweb_orm.Column("password", pweb_orm.String(250), nullable=False)
     address = pweb_orm.Column("address", pweb_orm.Text())
